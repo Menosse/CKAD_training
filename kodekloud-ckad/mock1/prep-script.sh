b27b4858da45bc8336e358434bdfd7f4bfc69cb1
root <famcsn2@gmail.com>
2020-02-19 04:01:42 +0000

    mock1

---
+++ kodekloud-ckad/mock1/prep-script.sh
@@ -0,0 +1,3 @@
+kubectl delete rs rs-d33393
+kubectl delete pod webapp-color
+kubectl delete pod app-sec-kff3345
---
+++ kodekloud-ckad/mock1/q11log.sh
@@ -0,0 +1,1 @@
+kubectl log e-com-1123 -ne-commerce > ../../../../opt/outputs/e-com-1123.logs
